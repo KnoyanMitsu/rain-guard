8445db3d119b171dd16052c3aed8fdcb209a1a13
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-13 16:01:09 +0700

    add overflow-hidden and relative

--- web/src/component/aestetic/AceUIAppname.tsx
+++ web/src/component/aestetic/AceUIAppname.tsx
@@ -7,9 +7,9 @@ type Props = {
 
 function AceUIAppname({ appname, description }: Props) {
   return (
-    <div className="overflow-hidden">
-      <div className="fixed top-100 justify-center left-28 w-100 h-100 bg-linear-to-r animate-move blur-3xl from-40% from-[#155dfd] via-[#eff6ff] via-10% to-[#155dfd] to-80% rounded-full"></div>
-      <div className="min-h-screen backdrop-blur-md p-10 flex flex-col justify-center">
+    <div className="relative overflow-hidden h-full">
+      <div className="absolute top-1/4 -left-10 w-96 h-96 bg-linear-to-r animate-move blur-3xl from-40% from-[#155dfd] via-[#eff6ff] via-10% to-[#155dfd] to-80% rounded-full"></div>
+      <div className="relative z-10 min-h-screen backdrop-blur-md p-10 flex flex-col justify-center">
         <div className="">
           <h1 className="font-bold text-5xl">{appname}</h1>
         </div>
--- web/src/component/template/AceUITemplateTwoGrid.tsx
+++ web/src/component/template/AceUITemplateTwoGrid.tsx
@@ -9,7 +9,7 @@ function AceUITemplateTwoGrid({ children }: AceUITemplateTwoGridProps) {
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-5 h-screen">
-      <div className="hidden md:block md:col-span-2">{childrenArray[0]}</div>
+      <div className="hidden md:block md:col-span-2 ">{childrenArray[0]}</div>
       <div className="md:col-span-3">{childrenArray[1]}</div>
     </div>
   );
