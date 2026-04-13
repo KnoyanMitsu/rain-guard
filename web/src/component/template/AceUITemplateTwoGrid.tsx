b2185cc63c2e132f9da61e0f17228699edba0d48
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-13 15:30:57 +0700

    responsive login page

--- web/src/component/template/AceUITemplateTwoGrid.tsx
+++ web/src/component/template/AceUITemplateTwoGrid.tsx
@@ -1,20 +1,18 @@
-import React from 'react'
+import React from "react";
 
 type AceUITemplateTwoGridProps = {
-  children: React.ReactNode
-}
-
-
+  children: React.ReactNode;
+};
 
 function AceUITemplateTwoGrid({ children }: AceUITemplateTwoGridProps) {
   const childrenArray = React.Children.toArray(children);
 
   return (
-    <div className='grid grid-cols-5  h-screen'>
-      <div className='col-span-2'>{childrenArray[0]}</div>
-      <div className='col-span-3'>{childrenArray[1]}</div>
+    <div className="grid grid-cols-1 md:grid-cols-5 h-screen">
+      <div className="hidden md:block md:col-span-2">{childrenArray[0]}</div>
+      <div className="md:col-span-3">{childrenArray[1]}</div>
     </div>
-  )
+  );
 }
 
-export default AceUITemplateTwoGrid
+export default AceUITemplateTwoGrid;
