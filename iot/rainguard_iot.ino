4bbb33f9d3f2b8450399989c6eb8ed2dbac756a2
TomiMartinoAffandi <tomimartino10@gmail.com>
2026-04-13 16:06:11 +0700

    fix(coba di wokwi)

--- iot/rainguard_iot.ino
+++ iot/rainguard_iot.ino
@@ -1,39 +1,73 @@
-#include <Arduino.h>
+// PIN SETUP (PIN ATAS ESP32)
+#define TRIG_PIN 12
+#define ECHO_PIN 13
 
-#define TRIG 5
-#define ECHO 18
-#define BUZZER 23
+#define LED_GREEN 27
+#define LED_YELLOW 26
+#define LED_RED 25
+#define BUZZER 14
 
 long duration;
 float distance;
-
-float threshold = 10;
+String statusBanjir;
 
 void setup() {
   Serial.begin(115200);
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
+
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
   pinMode(BUZZER, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(TRIG, LOW);
+float readDistance() {
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(TRIG, HIGH);
+  
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG_PIN, LOW);
 
-  duration = pulseIn(ECHO, HIGH);
+  duration = pulseIn(ECHO_PIN, HIGH);
   distance = duration * 0.034 / 2;
 
-  Serial.print("Distance: ");
-  Serial.println(distance);
+  return distance;
+}
 
-  if (distance < threshold) {
-    digitalWrite(BUZZER, HIGH);
+void loop() {
+  float d = readDistance();
+
+  // LOGIKA BANJIR
+  if (d > 200) {
+    statusBanjir = "Aman";
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_RED, LOW);
+    noTone(BUZZER);
+
+  } else if (d > 100) {
+    statusBanjir = "Siaga";
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_YELLOW, HIGH);
+    digitalWrite(LED_RED, LOW);
+    noTone(BUZZER);
+
   } else {
-    digitalWrite(BUZZER, LOW);
+    statusBanjir = "Bahaya";
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_RED, HIGH);
+    tone(BUZZER, 1000);
   }
 
-  delay(500);
+  // OUTPUT KE SERIAL (buat debug & demo)
+  Serial.print("Distance: ");
+  Serial.print(d);
+  Serial.print(" cm | Status: ");
+  Serial.println(statusBanjir);
+
+  delay(1000);
 }
