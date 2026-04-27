04bb7d3678fe23fc3caa76207fe661fb694c7a6f
Fabian Ananda Merdana <yanappuowo@yandex.com>
2026-04-27 13:48:21 +0700

    refactor: remove dark mode support and define static theme colors in CSS variables

--- web/src/component/aestetic/AceUIAppname.tsx
+++ web/src/component/aestetic/AceUIAppname.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 function AceUIAppname({ appname, description }: Props) {
   return (
-    <div className="relative overflow-hidden h-full bg-background dark:bg-text text-text dark:text-background">
+    <div className="relative overflow-hidden h-full bg-background text-text">
       <div className="absolute top-1/4 -left-10 w-96 h-96 bg-linear-to-r animate-move blur-3xl from-40% from-primary via-secondary via-10% to-accent to-80% rounded-full opacity-50 dark:opacity-30"></div>
       <div className="relative z-10 min-h-screen backdrop-blur-md p-10 flex flex-col justify-center">
         <div className="">
